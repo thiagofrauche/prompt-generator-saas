b51b41f47786f564a4663367a5b757842da2d3eb
Thiagofrauche <thiagofrauche@gmail.com>
2025-11-26 19:40:12 -0300

    Update supabaseClient.ts

--- lib/supabaseClient.ts
+++ lib/supabaseClient.ts
@@ -1,9 +1,8 @@
-'use client';
-import { createBrowserClient } from '@supabase/auth-helpers-nextjs';
+// lib/supabaseClient.ts
+import { createClient } from '@supabase/supabase-js';
 
-export function createSupabaseBrowser() {
-  return createBrowserClient(
-    process.env.NEXT_PUBLIC_SUPABASE_URL!,
-    process.env.NEXT_PUBLIC_SUPABASE_ANON_KEY!
-  );
-}
+const url = process.env.NEXT_PUBLIC_SUPABASE_URL!;
+const anonKey = process.env.NEXT_PUBLIC_SUPABASE_ANON_KEY!;
+
+export const supabase = createClient(url, anonKey);
+export default supabase;
