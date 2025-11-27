48e417cf58ec6a08238b0bb581d4bc613d51fa74
Thiagofrauche <thiagofrauche@gmail.com>
2025-11-27 12:53:10 -0300

    Update page.tsx

--- app/login/page.tsx
+++ app/login/page.tsx
@@ -1,6 +1,7 @@
+'use client';
+
 export const dynamic = 'force-dynamic';
 export const revalidate = 0;
-'use client';
 
 import { useState } from 'react';
 import { createBrowserClient } from '@supabase/ssr';
